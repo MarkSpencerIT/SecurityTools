64fdabb182a527530b864c90b3215fa4b457b56f
Mark Spencer <64541554+MarkSpencerIT@users.noreply.github.com>
2020-12-31 14:01:08 +0000

    Update AppacheVersion.py

--- AppacheVersion.py
+++ AppacheVersion.py
@@ -7,16 +7,27 @@
 
 import urllib.request
 
+from datetime import date
+today = date.today()
+print("Today's date:", today)
+
 response1 = urllib.request.urlopen('https://URL1/')
 response2 = urllib.request.urlopen('https://URL2/')
 
+Date = (today)
 Value1 = ("URL1 - ",response1.headers['Server'])
 Value2 = ("URL2 - ",response2.headers['Server'])
 
 print("URL1 - ",response1.headers['Server'])
 print("URL2 - ",response2.headers['Server'])
 
-with open('/LOCATION/list_1.txt', 'w') as output:
+with open('/LOCATION/Webversion' + (str(Date)) + '.txt', 'w') as output:
+    file.write("Today's Date - ")
+    output.write("\n")
+    file.write("URL1 - ")
     output.write(str(Value1))
     output.write("\n")
+    file.write("URL2 - ")
     output.write(str(Value2)
+    output.write("\n")
+            
