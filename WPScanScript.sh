d59b0798baf0bbddbb8851e6d26ea86d26bd233f
Mark Spencer <64541554+MarkSpencerIT@users.noreply.github.com>
2021-07-26 12:22:51 +0100

    Update WPScanScript.sh

--- WPScanScript.sh
+++ WPScanScript.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+wpscan --update                                                                                                                                                                                      1 ⨯
+
 wpscan --url site1.com -o /home/kalims/Documents/site1.txt
 wpscan --url site2.co.uk -o /home/kalims/Documents/site2.txt
 
 echo "First site"
-grep "The version is out of date" /home/kalims/Documents/markspencerit.txt
+grep "The version is out of date" /home/kalims/Documents/site1.txt
 echo "second site"
-grep "The version is out of date" /home/kalims/Documents/BradfordMMA.txt
+grep "The version is out of date" /home/kalims/Documents/site2.txt
 
 echo "done"
