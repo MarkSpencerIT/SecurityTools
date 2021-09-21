bb1625ea4b3ad1d46d58eff461711257e34634ee
Mark Spencer <64541554+MarkSpencerIT@users.noreply.github.com>
2021-09-21 15:28:15 +0100

    Update WPScanScript.sh
    Added greps to find plugin vulnerabilities, version of wordpress and URL.
    Added write the greps to a summary file (sum.txt)

--- WPScanScript.sh
+++ WPScanScript.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+wpscan --update
 
-wpscan --update                                                                                                                                                                                      1 ⨯
+echo "Scanning - First site with WPScan"
 
 wpscan --url site1.com --ignore-main-redirect -o /home/USER/Documents/site1.txt
 
-echo "Time: $(date -Iseconds). WPScan." >> /home/USER/Documents/sum.txt | awk '{print $0,"\n"}'
+echo "Completed Scan on First site"
 
-echo "First site"
+echo "Time: $(date -Iseconds). WPScan." > /home/USER/Documents/sum.txt | awk '{print $0,"\n"}'
+
+echo "Adding details to summary document - First site"
+
 echo "First site" >> /home/USER/Documents/sum.txt | awk '{print $0,"\n"}'
+echo "------------" >> /home/USER/Documents/sum.txt | awk '{print $0,"\n"}'
+grep "URL:" /home/USER/Documents/site1.txt >> /home/USER/Documents/sum.txt | awk '{print $0,"\n"}'
+echo "------------" >> /home/USER/Documents/sum.txt | awk '{print $0,"\n"}'
 grep "WordPress version" /home/USER/Documents/site1.txt >> /home/USER/Documents/sum.txt | awk '{print $0,"\n"}'
-grep "Effective URL" /home/USER/Documents/site1.txt >> /home/USER/Documents/sum.txt | awk '{print $0,"\n"}'
+echo "------------" >> /home/USER/Documents/sum.txt | awk '{print $0,"\n"}'
 grep -i -B 4 "The version is out of date" /home/USER/Documents/site1.txt >> /home/USER/Documents/sum.txt | awk '{print $0,"\n"}'
 
-echo "done"
+echo "detials added to the summary document sum.txt"
+
+echo "Scan complete your files are save here /home/USER/Documents/"
+echo "NOTE - Make sure you have changes the USER section /home/USER/Documents/"
